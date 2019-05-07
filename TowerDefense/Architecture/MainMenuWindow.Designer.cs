b7b17dba799493519e6f956f9ef8504f97d36591
FirsovaA <firsovasasha123@gmail.com>
2019-05-07 22:31:32 +0430

    Main menu design

--- TowerDefense/Architecture/MainMenuWindow.Designer.cs
+++ TowerDefense/Architecture/MainMenuWindow.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace TowerDefense.Architecture
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TowerDefense.Architecture
 {
     partial class MainMenuWindow
     {
@@ -28,51 +31,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuWindow));
             this.Start = new System.Windows.Forms.Button();
             this.Exit = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Start
             // 
+            this.Start.BackColor = System.Drawing.Color.LavenderBlush;
             this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.Start.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.Start.Location = new System.Drawing.Point(151, 180);
+            this.Start.Font = new System.Drawing.Font("NSimSun", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Start.Location = new System.Drawing.Point(206, 162);
             this.Start.Name = "Start";
-            this.Start.Size = new System.Drawing.Size(88, 39);
+            this.Start.Size = new System.Drawing.Size(120, 39);
             this.Start.TabIndex = 0;
             this.Start.Text = "Start";
-            this.Start.UseVisualStyleBackColor = true;
+            this.Start.UseVisualStyleBackColor = false;
             this.Start.Click += new System.EventHandler(this.Start_Click);
             // 
             // Exit
             // 
-            this.Exit.Location = new System.Drawing.Point(151, 234);
+            this.Exit.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Exit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Exit.Font = new System.Drawing.Font("NSimSun", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Exit.Location = new System.Drawing.Point(206, 207);
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(88, 42);
+            this.Exit.Size = new System.Drawing.Size(120, 39);
             this.Exit.TabIndex = 1;
             this.Exit.Text = "Exit";
-            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.UseVisualStyleBackColor = false;
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 0;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::TowerDefense.Properties.Resources.castle;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 191);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 281);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.Transparent;
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Title.Font = new System.Drawing.Font("Palace Script MT", 31.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.Title.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Title.Location = new System.Drawing.Point(12, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(358, 50);
+            this.Title.TabIndex = 3;
+            this.Title.Text = "We are sorry in advance";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // MainMenuWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 440);
-            this.Controls.Add(this.label1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::TowerDefense.Properties.Resources.original;
+            this.ClientSize = new System.Drawing.Size(372, 453);
+            this.Controls.Add(this.Title);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainMenuWindow";
-            this.Text = "Form1";
+            this.Text = "Haven\'t thought bout the title";
             this.Load += new System.EventHandler(this.MainMenuWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,6 +115,7 @@
 
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Button Exit;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Label Title;
     }
 }
