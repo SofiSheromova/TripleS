2e0c67d3cab56be9c063d48b7f52adc6b74ac8d0
sofi.sher <sofi.sheromova@gmail.com>
2019-05-13 15:56:53 +0500

    Обычный Дейкстра

--- TowerDefense/PathFinder.cs
+++ TowerDefense/PathFinder.cs
@@ -14,50 +14,40 @@ namespace TowerDefense
 
     public static class DijkstraPathFinder
     {
-        private static readonly Point[] Directions =
-        {
-            new Point(0, -1),
-            new Point(0, 1),
-            new Point(-1, 0),
-            new Point(1, 0)
-        };
-
         public static List<Point> Dijkstra(Point start, Point end, Game game)
         {
-            var notVisited = new List<Point> {start};
+            var notVisited = GetPointMap(game.MapWidth, game.MapHeight);
             var track = new Dictionary<Point, DijkstraData>
                 {[start] = new DijkstraData {Previous = new Point(-1, -1), Price = 0}};
+
             while (true)
             {
-                var toOpen = default(Point);
+                var toOpen = new Point(-1, -1);
                 var bestPrice = double.PositiveInfinity;
-                foreach (var e in notVisited)
+                foreach (var point in notVisited)
                 {
-                    if (track.ContainsKey(e) && track[e].Price < bestPrice)
+                    if (track.ContainsKey(point) && track[point].Price < bestPrice)
                     {
-                        bestPrice = track[e].Price;
-                        toOpen = e;
+                        bestPrice = track[point].Price;
+                        toOpen = point;
                     }
                 }
 
                 if (toOpen == end) break;
+                //if (toOpen.X == -1) break;
 
-                foreach (var e in Directions.Select(x => new Point(x.X + toOpen.X, x.Y + toOpen.Y)))
+                foreach (var point in GetIncidentPoint(toOpen, game.MapWidth, game.MapHeight))
                 {
-                    if (e.X < 0 || e.X >= game.MapWidth || e.Y < 0 || e.Y >= game.MapHeight)
-                        continue;
-                    var currentPrice = track[toOpen].Price + game.Map[e.X, e.Y]?.Live ?? 0;
-                    if (!track.ContainsKey(e) || track[e].Price > currentPrice)
-                    {
-                        track[e] = new DijkstraData {Previous = toOpen, Price = currentPrice};
-                        notVisited.Add(e);
-                    }
+                    var currentPrice = track[toOpen].Price + game.Map[point.X, point.Y]?.Live ?? 0;
+                    if (!track.ContainsKey(point) || track[point].Price > currentPrice)
+                        track[point] = new DijkstraData {Previous = toOpen, Price = currentPrice};
                 }
 
                 notVisited.Remove(toOpen);
             }
 
             var result = new List<Point>();
+
             while (end.X != -1)
             {
                 result.Add(end);
@@ -65,6 +55,25 @@ namespace TowerDefense
             }
 
             result.Reverse();
+            return result;
+        }
+
+        private static IEnumerable<Point> GetIncidentPoint(Point point, int mapWidth, int mapHeight)
+        {
+            Tuple<int, int>[] delta =
+                {Tuple.Create(0, 1), Tuple.Create(1, 0), Tuple.Create(-1, 0), Tuple.Create(0, -1)};
+            foreach (var d in delta)
+                if (point.X + d.Item1 < mapWidth && point.X + d.Item1 >= 0 && point.Y + d.Item2 < mapHeight
+                    && point.Y + d.Item2 >= 0)
+                    yield return new Point(point.X + d.Item1, point.Y + d.Item2);
+        }
+
+        private static List<Point> GetPointMap(int width, int height)
+        {
+            var result = new List<Point>();
+            for (var x = 0; x < width; x++)
+            for (var y = 0; y < height; y++)
+                result.Add(new Point(x, y));
             return result;
         }
     }
